1f0b5a133c660b1cc9319cc27c1af9f7c026de83
Aishani Dandapure <aishani.dandapure@gmail.com>
2023-09-19 11:08:10 -0700

    Added some new notes and Python files

---
+++ Notes/1 - Natural Language Processing.md
@@ -0,0 +1,14 @@
+
+# Output
+
+# Comments 
+Comments are pieces of tect that are *NOT* interpreted
+This means that the text is ignored
+We use the # symbol to make comments 
+
+# [[Headers]]
+
+The first three lines of code in our files are:
+	# Title
+	# Author
+	# Date
---
+++ quotegenerator.py
@@ -0,0 +1,18 @@
+# Quote Generator
+# Author: Aishani
+# Date: 19 September 2023
+
+# Output a quote from someone famous 
+print("You miss 100 percent of the shots you don't take. -- Wayne Gretzky")
+
+# Print out another quote
+print("There's no time like the present")
+
+# Print out another
+print("Don't worry, be happy. -- Bobby McFerrin")
+
+# Ask the user a question 
+input("What was your favourite quote? ")
+
+# Respond
+print("That's my favourite too!")
