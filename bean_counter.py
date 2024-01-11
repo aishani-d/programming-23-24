e68e31b1efc88623c90c1a02fb499cd9ec25da18
Aishani Dandapure <aishani.dandapure@gmail.com>
2024-01-11 11:39:56 -0800

    winter holiday

---
+++ bean_counter.py
@@ -0,0 +1,21 @@
+# Jelly Bean Colour Counter
+# Author: Ubial
+# 9 January 2024
+
+# Version 0.1
+#  - Count red pixels/beans
+#  - Report on the percentage of all red beans
+
+# Visit every pixel in the image
+for y in range(jelly_bean_img.height):
+    for x in range(jelly_bean_img.width):
+    # Get the pixel information
+        current_pixel = jelly_bean_img.getpixel((x,y))
+    # If this pixel is red
+        if colour_helper
+        # Store its location somewhere
+
+# Count all the locations of red pixels
+# Divide by the total pixels in the image
+
+# Generate the report
